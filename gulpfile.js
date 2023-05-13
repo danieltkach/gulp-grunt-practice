3dce25a9ec4ddbbf42239e3682e90a87f25ff8a1
Daniel Tkach <danieltkach@gmail.com>
2023-05-13 12:41:03 -0300

    fix/ browserify path for main.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,50 @@
 / concatenating, and minifying your JavaScript files.
 */
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var babel = require('gulp-babel');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+const path = require('path');
+const mainJsPath = path.resolve(__dirname, 'src/scripts/main.js');
 
-gulp.task('styles', function() {
+// Require the needed packages
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const babelify = require('babelify');
+
+// Define the styles task
+function styles() {
   return gulp.src('src/styles/*.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest('dist/styles'));
-});
+}
 
-gulp.task('scripts', function() {
-  return gulp.src('src/scripts/*.js')
-  .pipe(babel({
-    presets: ['@babel/preset-env']
-  }))
-  .pipe(concat('main.js'))
-  .pipe(gulp.dest('dist/script'))
-  .pipe(rename('main.min.js'))
-  .pipe(uglify())
-  .pipe(gulp.dest('dist/scripts'));
-});
+// Define the scripts task
+function scripts() {
+  return browserify(mainJsPath)
+    .transform(babelify.configure({
+      presets: ["@babel/preset-env"]
+    }))
+    .bundle()
+    .pipe(source('main.min.js')) // Gives streaming vinyl file object
+    .pipe(buffer()) // Convert from streaming to buffered vinyl file object
+    .pipe(uglify()) // Now gulp-uglify works
+    .pipe(gulp.dest('dist/scripts'));
+}
 
-gulp.task('default', gulp.parallel('styles', 'scripts'));
+// Define the HTML task
+function html() {
+  return gulp.src('src/*.html')
+  .pipe(gulp.dest('dist'));
+}
+
+// Watch for changes
+function watch() {
+  gulp.watch('src/styles/**/*.scss', styles);
+  gulp.watch('src/scripts/**/*.js', scripts);
+  gulp.watch('src/*.html', html);
+}
+
+// Define the default task
+gulp.task('default', gulp.series(styles, scripts, html, watch));
