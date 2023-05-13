015da7679f10dbd99a80814459807e0999dde3e2
Daniel Tkach <danieltkach@gmail.com>
2023-05-13 13:45:52 -0300

    imagemin, reloading, testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 /*
-/ This gulpfile.js sets up two tasks: one for processing your
-/ SASS files into CSS, and another for transpiling, 
-/ concatenating, and minifying your JavaScript files.
+/ This gulpfile.js sets up tasks for processing your
+/ SASS files into CSS, for transpiling, concatenating, and 
+/ minifying your JavaScript files, compressing images, testing 
+/ and so on.
 */
-
-const path = require('path');
-const mainJsPath = path.resolve(__dirname, 'src/scripts/main.js');
 
 // Require the needed packages
 const gulp = require('gulp');
@@ -20,20 +18,25 @@ const babelify = require('babelify');
 function styles() {
   return gulp.src('src/styles/*.scss')
   .pipe(sass().on('error', sass.logError))
+  .pipe(cleanCSS())
   .pipe(gulp.dest('dist/styles'));
 }
+
+// browserify won't take relative paths, so these are necessary
+const path = require('path');
+const mainJsPath = path.resolve(__dirname, 'src/scripts/main.js');
 
 // Define the scripts task
 function scripts() {
   return browserify(mainJsPath)
-    .transform(babelify.configure({
-      presets: ["@babel/preset-env"]
-    }))
-    .bundle()
-    .pipe(source('main.min.js')) // Gives streaming vinyl file object
-    .pipe(buffer()) // Convert from streaming to buffered vinyl file object
-    .pipe(uglify()) // Now gulp-uglify works
-    .pipe(gulp.dest('dist/scripts'));
+  .transform(babelify.configure({
+    presets: ["@babel/preset-env"]
+  }))
+  .bundle()
+  .pipe(source('main.min.js')) // Gives streaming vinyl file object
+  .pipe(buffer()) // Convert from streaming to buffered vinyl file object
+  .pipe(uglify()) // Now gulp-uglify works
+  .pipe(gulp.dest('dist/scripts'));
 }
 
 // Define the HTML task
@@ -42,12 +45,51 @@ function html() {
   .pipe(gulp.dest('dist'));
 }
 
-// Watch for changes
-function watch() {
-  gulp.watch('src/styles/**/*.scss', styles);
-  gulp.watch('src/scripts/**/*.js', scripts);
-  gulp.watch('src/*.html', html);
+// Imagemin task
+const imagemin = require('gulp-imagemin');
+const images =()=> {
+  return gulp.src('src/images/**/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images'));
 }
 
+// Live Reloading
+const browserSync = require('browser-sync').create();
+const serve = (done) => {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+  done();
+}
+
+const reload = (done) => {
+  browserSync.reload();
+  done();
+}
+
+// Testing
+const jasmine = require('gulp-jasmine');
+const test =()=> {
+  return gulp.src('test/**/*.js')
+  .pipe(jasmine());
+}
+
+// Watch for changes
+function watch() {
+  gulp.watch('src/styles/**/*.scss', gulp.series(styles, reload));
+  gulp.watch('src/scripts/**/*.js', gulp.series(scripts, reload));
+  gulp.watch('src/*.html', gulp.series(html, reload));
+  gulp.watch('src/images/**/*', gulp.series(images, reload))
+}
+
+// Export all tasks to run individually by gulp <task-name>
+exports.styles = styles;
+exports.scripts = scripts;
+exports.images = images;
+exports.serve = gulp.series(styles, scripts, html, images, serve);
+exports.test = test;
+
 // Define the default task
-gulp.task('default', gulp.series(styles, scripts, html, watch));
+exports.default = gulp.series(styles, scripts, html, images, serve, watch);
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,6 @@
 export const greet = () => 'Hello, Gulp & Grunt!';
+
+window.onload =()=> {
+  document.getElementById('greeting')
+    .textContent = greet();
+}
